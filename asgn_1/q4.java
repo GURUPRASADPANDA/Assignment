4c92af94adfd58189d9e3e500533b30673218e4f
GURU PRASAD PANDA <137532628+GURUPRASADPANDA@users.noreply.github.com>
2024-04-18 00:50:04 +0530

    Update q4.java

--- asgn_1/q4.java
+++ asgn_1/q4.java
@@ -12,7 +12,7 @@ public class q4 {
 					for(int l = 0 ; l<arr.length ; l++) {
 						for(int m = 0 ; m<arr.length ; m++){
 							for(int n = 0 ; n<arr.length ; n++) {
-								for(int o = 0 ; o<arr.length ; o++) {
+								
 									if(i!=j && j!=k && k!=l && l!=m && m!=n && n!=o && i!=k &&
 											i!=l && i!=m && i!=n && i!=o && j!=m && j!=n && j!=o 
 											&& k!=m && k!=n && k!=o && l!=n && l!=o && m!=o) {
@@ -25,7 +25,7 @@ public class q4 {
 									System.out.println(arr[n]);
 									
 									}
-								}
+								
 							
 							}
 						}
