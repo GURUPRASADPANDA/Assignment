571db51a383afc69bfc241c53788a135fd8ceaae
GURU PRASAD PANDA <137532628+GURUPRASADPANDA@users.noreply.github.com>
2024-04-18 01:30:13 +0530

    Update q2.java

--- asgn_1/q2.java
+++ asgn_1/q2.java
@@ -1,18 +1,33 @@
+import java.util.*;
 
-public class q1 {
+public class Q2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-      int no = Integer.parseInt(args[0]);
-      
-      System.out.println("entered no is : "+no);
-      int count = 0;
-      while(no>2) {
-    	  no=no/2;
-    	  count++;
-      }
-      System.out.println(count);
+          Scanner sc = new Scanner(System.in);
+          System.out.println("Enter your weight in kg ");
+          double weight = sc.nextDouble();
+          System.out.println("Enter your height in metre ");
+          double height = sc.nextDouble();
+          double bmi = weight/(Math.pow(height,2));
+          System.out.println("BMI:"+bmi);
+		  if(bmi<18.5)
+		  {
+			 System.out.println(" Underweight "); 
+		  }
+		  else if(bmi >= 18.5 && bmi <= 24.9)
+		  {
+			  System.out.println(" Normal weight "); 
+		  }
+		  else if(bmi >= 25.0 && bmi <= 29.9)
+		  {
+			  System.out.println("Overweight ");
+		  }
+		  else if(bmi >= 30.0)
+		  {
+			  System.out.println("Obese ");
+		  }
+		  else
+			  System.out.println("you have to input valid data");
 	}
+} 
 
-}
