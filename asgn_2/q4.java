1f12a67d66deb3c11246de806714ed4dd05df785
GURU PRASAD PANDA <137532628+GURUPRASADPANDA@users.noreply.github.com>
2024-06-11 20:26:38 +0530

    Update q4.java

--- asgn_2/q4.java
+++ asgn_2/q4.java
@@ -1,12 +1,11 @@
-class Deposit{
+class Deposit
+{
     long Principal;
     int Time;
     double Rate;
     double TotalAmt; 
- 
     Deposit ( )
     {
-     
     }
     Deposit (long Principal, int Time, double Rate)
     {
@@ -30,18 +29,16 @@ class Deposit{
         System.out.println("Time :"+Time);
         System.out.println("Rate :"Rate);
         System.out.println("Total amount is :"+TotalAmt);
-        
     }
     void calcAmt( )
     {
         this.TotalAmt = (double)Principal + (Principal*Time)/100;
     }
 }
-
-
-
-class q4 {
-    public static void main(String[] args) {
+class q4 
+{
+    public static void main(String[] args)
+    {
         Deposit d1 = new Deposit(1000,2,5);
         d1.calcAmt();
         d1.display();
