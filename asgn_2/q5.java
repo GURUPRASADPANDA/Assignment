108f00bb978b998b82887af81781e1d2ed914fbf
GURU PRASAD PANDA <137532628+GURUPRASADPANDA@users.noreply.github.com>
2024-06-11 20:30:16 +0530

    Update q5.java

--- asgn_2/q5.java
+++ asgn_2/q5.java
@@ -1,7 +1,9 @@
-class Person{
+class Person
+{
     String name;
     int age;
-    Person(){
+    Person()
+    {
     }
     Person (String name, int age)
     {
@@ -9,9 +11,10 @@ class Person{
       this.age = age;
     }
 } 
-class Employee extends Person{
-     int Eid;
-     double salary;
+class Employee extends Person
+{
+    int Eid;
+    double salary;
     Employee (String name, int age , int Eid, double salary)
     {
       super(name,age);
@@ -22,10 +25,11 @@ class Employee extends Person{
     {
       System.out.println("name is: "+name+"\nage is: "+age+"\nEid is: "+Eid+"\nsalary is: "+salary);
     }
-
 }
- class q5 {
-    public static void main(String[] args) {
+ class q5 
+{
+    public static void main(String[] args) 
+    {
         Employee e1 = new Employee("Guru", 20, 6, 100000000);
         e1.empDisplay();
     }
